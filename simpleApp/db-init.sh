c69199ef57a77026223d506f85ec40f875f80db0
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 14:25:25 +0200

    Update db-init.sh

--- simpleApp/db-init.sh
+++ simpleApp/db-init.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+sudo su -
 source /usr/local/osmosix/etc/userenv
-sudo curl https://raw.githubusercontent.com/yannickcastano/cloudcenter/master/simpleApp/db-init.sql --output /tmp/db-init.sql
+curl https://raw.githubusercontent.com/yannickcastano/cloudcenter/master/simpleApp/db-init.sql --output /tmp/db-init.sql
+echo "[mysqld]" >> /etc/mysql/my.cnf
+echo "skip-grant-tables" >> /etc/mysql/my.cnf
+service mysql restart
