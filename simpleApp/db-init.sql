b8ac2ea3911c5a1c62ab0e972db8b565bb4d0e99
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 13:55:51 +0200

    Create db-init.sql

---
+++ simpleApp/db-init.sql
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS `simpleAppDB`;
+GRANT ALL PRIVILEGES ON simpleAppDB.* TO 'admin'@'%' IDENTIFIED BY '$ecurity01' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON simpleAppDB.* TO 'admin'@'localhost' IDENTIFIED BY '$ecurity01' WITH GRANT OPTION;
+
+USE simpleAppDB;
+
+CREATE TABLE IF NOT EXISTS people (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  INDEX(last_name)
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS styles (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  style VARCHAR(30),
+  INDEX(style)
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS restaurants (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  type_id INT(4) UNSIGNED NOT NULL,
+  name VARCHAR(30),
+  address VARCHAR(255),
+  city VARCHAR(80),
+  telephone VARCHAR(20),
+  INDEX(name),
+  FOREIGN KEY (type_id) REFERENCES styles(id)
+) engine=InnoDB;
+
+CREATE TABLE IF NOT EXISTS visits (
+  id INT(4) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  restaurant_id INT(4) UNSIGNED NOT NULL,
+  people_id INT(4) UNSIGNED NOT NULL,
+  visit_date DATE,
+  comment VARCHAR(255),
+  FOREIGN KEY (restaurant_id) REFERENCES restaurants(id),
+  FOREIGN KEY (people_id) REFERENCES people(id)
+) engine=InnoDB;
+
