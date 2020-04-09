0997fdd880a86a6f794deccd1ecec58c9d001b92
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 19:04:50 +0200

    Update app-init.sh

--- simpleApp/app-init.sh
+++ simpleApp/app-init.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /usr/local/osmosix/etc/userenv
+#-- restart apache to get php lib loadded
+sudo service apache2 restart
+#-- download php app
 sudo curl https://raw.githubusercontent.com/yannickcastano/cloudcenter/master/simpleApp/app.php --output /var/www/html/app.php
