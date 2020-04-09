41633b5fd291da41243cea3ddc9192bdd2bfd6a0
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 19:33:33 +0200

    Create db-root-access.sql

---
+++ simpleApp/db-root-access.sql
@@ -0,0 +1,5 @@
+USE mysql;
+UPDATE user set authentication_string=PASSWORD("S3cur1ty01") where User='root';
+UPDATE user set plugin="mysql_native_password" where User='root';
+FLUSH privileges;
+QUIT
