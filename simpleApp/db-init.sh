b7632120aac59f8346e2d88d662d93220a4c61d5
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 19:36:11 +0200

    Update db-init.sh

--- simpleApp/db-init.sh
+++ simpleApp/db-init.sh
@@ -3,17 +3,13 @@ source /usr/local/osmosix/etc/userenv
 
 #-- download database config init file
 curl https://raw.githubusercontent.com/yannickcastano/cloudcenter/master/simpleApp/db-init.sql --output /tmp/db-init.sql
+curl https://raw.githubusercontent.com/yannickcastano/cloudcenter/master/simpleApp/db-root-access.sql --output /tmp/db-root-access.sql
 
 #-- change MySQL root password 
 sudo bash -c 'echo "[mysqld]" >> /etc/mysql/my.cnf'
 sudo bash -c 'echo "skip-grant-tables" >> /etc/mysql/my.cnf'
 sudo service mysql restart
-mysql -u root
-USE mysql;
-UPDATE user set authentication_string=PASSWORD("S3cur1ty01") where User='root';
-UPDATE user set plugin="mysql_native_password" where User='root';
-FLUSH privileges;
-QUIT
+mysql -u root < /tmp/db-root-access.sql
 sudo sed -i '/skip-grant-tables/d' /etc/mysql/my.cnf
 sudo service mysql restart
 
