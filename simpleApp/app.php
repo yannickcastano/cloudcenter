73efeaf83ca141977de231f90f9e684ab4f79968
yannickcastano <39559205+yannickcastano@users.noreply.github.com>
2020-04-09 18:12:26 +0200

    Update app.php

--- simpleApp/app.php
+++ simpleApp/app.php
@@ -1,9 +1,59 @@
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+}
+</style>
+</head>
 <body>
 
 <?php
-echo "My simple App";
+echo "My simple App</br>";
+//Open userenv file and search for DB IP address
+$myfile = fopen("/usr/local/osmosix/etc/uservenv", "r") or die("Unable to open file!");
+while(!feof($myfile)) {
+  $myline = fgets($myfile);
+  if (strpos($myline, "CliqrDependencies")) {
+    $pos = strpos($myline, "=");
+    $dbhost = substr($myline,$pos+2,-1);
+    echo "DEBUG: DB host is: ".$dbhost."</br>";
+  }
+  if (strpos($myline, "CliqrTier_".$dbhost."_PUBLIC_IP")) {
+    $pos = strpos($myline, "=");
+    $dbip = substr($myline,$pos+2,-1);
+    echo "DEBUG: DB IP is: ".$dbip."</br>";
+  }
+}
+fclose($myfile);
+  
+$username = "admin";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($dbip, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, firstname, lastname FROM MyGuests";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["firstname"]. " " . $row["lastname"]. "</td></tr>";
+    }
+    echo "</table>";
+} else {
+    echo "0 results";
+}
+
+$conn->close();
 ?> 
 
 </body>
