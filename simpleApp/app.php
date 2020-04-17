acf54834f0c4b52693fcfa9ad9dd5f856d549a5e
yacastan <yacastan@cisco.com>
2020-04-17 10:04:53 +0200

    .

--- simpleApp/app.php
+++ simpleApp/app.php
@@ -97,7 +97,7 @@ function aci_endpoint_extract($endpoint_ip){
   $endpoint_info = aci_get('node/class/fvCEp.json?query-target-filter=eq(fvCEp.ip,"'.$endpoint_ip.'")');
   echo '</br>DEBUG - Endpoint info: ';
   print_r($endpoint_info);
-  echo '</br>DEBUG - Endpoint DN: '.$endpoint_info["imdata"][0]["fvCEp"]["attributes"]["annotation"];
+  echo '</br>DEBUG - Endpoint DN: '.$endpoint_info["imdata"][0]["fvCEp"]["attributes"]["annotation"]["childAction"]["contName"]["dn"];
 
 }
 
